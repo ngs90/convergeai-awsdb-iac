d9db89bd5d5deccaa42ecc0148859236d65083f1
ngs90 <nicolaj.schmit@hotmail.com>
2025-02-21 12:17:52 +0100

    setup databricks workspace provider

--- aws-workspace-basic/policies.tf
+++ aws-workspace-basic/policies.tf
@@ -1,5 +1,6 @@
 
 resource "databricks_cluster_policy" "small_compute_policy" {
+  provider   = databricks.workspace
   name       = "Small Compute Policy"
   definition = jsonencode(merge(local.default_compute_policy, var.small_compute_policy))
   depends_on = [data.databricks_mws_workspaces.this]
